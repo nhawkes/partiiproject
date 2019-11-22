a8c43d9e2d1fc8b5644740090306627ff623c4c3
nh487 <nh487@cam.ac.uk>
2019-11-22 22:50:37 +0000

    Update test

--- Compiler.Tests/Tests.fs
+++ Compiler.Tests/Tests.fs
@@ -7,72 +7,6 @@ open Wasm
 open Stg
 open WasmGen
 
-let wasmModule =
-    [ TypeSec [ [ I32 ], [ I32 ] ]
-      FuncSec [ 0u ]
-      ExportSec
-          [ { nm = "Fibonacci"
-              exportdesc = ExportFunc 0u } ]
-      CodeSec
-          [ [],
-            [ LocalGet 0u
-              I32Const 0
-              I32Eq
-              IfElse
-                  ([ I32 ], [ I32Const 1 ],
-                   [ LocalGet 0u
-                     I32Const 1
-                     I32Eq
-                     IfElse
-                         ([ I32 ], [ I32Const 1 ],
-                          [ LocalGet 0u
-                            I32Const 1
-                            I32Sub
-                            Call 0u
-
-                            LocalGet 0u
-                            I32Const 2
-                            I32Sub
-                            Call 0u
-
-                            I32Add ]) ]) ] ] ]
-
-
-let stgModule: Program<Var> =
-    [ (UserVar "Fibonacci"),
-      TopLam
-          (([ (UserVar "x") ], [],
-            [ (UserVar "a")
-              (UserVar "b") ], []),
-           Case
-               (Prim [ AVar(UserVar "x") ], (UserVar "x"),
-                PAlts
-                    ([ I32Const 0, Prim [ ALit(I32Const 1) ] ],
-                     Case
-                         (Prim [ AVar(UserVar "x") ], (UserVar "x"),
-                          PAlts
-                              ([ I32Const 1, Prim [ ALit(I32Const 1) ] ],
-                               Case
-                                   (Prim
-                                       [ AVar(UserVar "Fibonacci")
-                                         ALit I32Sub
-                                         ALit(I32Const 1)
-                                         AVar(UserVar "x") ], (UserVar "a"),
-                                    PAlts
-                                        ([],
-                                         Case
-                                             (Prim
-                                                 [ AVar(UserVar "Fibonacci")
-                                                   ALit(I32Sub)
-                                                   ALit(I32Const 2)
-                                                   AVar((UserVar "x")) ], (UserVar "b"),
-                                              PAlts
-                                                  ([],
-                                                   Prim
-                                                       [ ALit I32Add
-                                                         AVar(UserVar "a")
-                                                         AVar(UserVar "b") ]))))))))) ]
-
 let arrayFromTuple =
     function
     | null -> [||]
@@ -91,6 +25,35 @@ let compile byteList =
 
 [<Fact>]
 let ``WASM Fibinacci 7``() =
+    let wasmModule =
+        [ TypeSec [ [ I32 ], [ I32 ] ]
+          FuncSec [ 0u ]
+          ExportSec
+              [ { nm = "Fibonacci"
+                  exportdesc = ExportFunc 0u } ]
+          CodeSec
+              [ [],
+                [ LocalGet 0u
+                  I32Const 0
+                  I32Eq
+                  IfElse
+                      ([ I32 ], [ I32Const 1 ],
+                       [ LocalGet 0u
+                         I32Const 1
+                         I32Eq
+                         IfElse
+                             ([ I32 ], [ I32Const 1 ],
+                              [ LocalGet 0u
+                                I32Const 1
+                                I32Sub
+                                Call 0u
+
+                                LocalGet 0u
+                                I32Const 2
+                                I32Sub
+                                Call 0u
+
+                                I32Add ]) ]) ] ] ]
     let fibonacciProgram = emitWasmModule wasmModule |> compile
     let output = fibonacciProgram.Exports?Fibonacci (7)
     Assert.Equal(21, output)
@@ -139,11 +102,191 @@ let ``Boxed Int``() =
 
     let (output: Int32) = boxedIntProgram.Exports?Int (7)
     let memory = [ 0 .. 8 ] |> List.map ((*) 4 >> boxedIntProgram.Exports?Get)
-    Assert.StrictEqual("[3; 0; 2; 7; 0; 0; 0; 0; 0]", sprintf "%A" memory)
+    Assert.StrictEqual("[12; 0; 2; 7; 0; 0; 0; 0; 0]", sprintf "%A" memory)
 
 
 [<Fact>]
 let ``STG Fibinacci 7``() =
+    let stgModule: Program<Var> =
+        [ (UserVar "Fibonacci"),
+          TopLam
+              (([ (UserVar "x") ], [],
+                [ (UserVar "a")
+                  (UserVar "b") ], []),
+               Case
+                   (Prim [ AVar(UserVar "x") ], (UserVar "x"),
+                    PAlts
+                        ([ I32Const 0, Prim [ ALit(I32Const 1) ] ],
+                         Case
+                             (Prim [ AVar(UserVar "x") ], (UserVar "x"),
+                              PAlts
+                                  ([ I32Const 1, Prim [ ALit(I32Const 1) ] ],
+                                   Case
+                                       (Prim
+                                           [ AVar(UserVar "Fibonacci")
+                                             ALit I32Sub
+                                             ALit(I32Const 1)
+                                             AVar(UserVar "x") ], (UserVar "a"),
+                                        PAlts
+                                            ([],
+                                             Case
+                                                 (Prim
+                                                     [ AVar(UserVar "Fibonacci")
+                                                       ALit(I32Sub)
+                                                       ALit(I32Const 2)
+                                                       AVar((UserVar "x")) ], (UserVar "b"),
+                                                  PAlts
+                                                      ([],
+                                                       Prim
+                                                           [ ALit I32Add
+                                                             AVar(UserVar "a")
+                                                             AVar(UserVar "b") ]))))))))) ]
+    let fibonacciProgram =
+        stgModule
+        |> WasmGen.genProgram
+        |> emitWasmModule
+        |> compile
+
+    let output = fibonacciProgram.Exports?Fibonacci (7)
+    Assert.Equal(21, output)
+
+
+[<Fact>]
+let ``STG Boxed Fibinacci 7``() =    
+    let stgModule : Program<Var> =
+        [
+            (UserVar "Int", TopConstr [UserVar "I"])
+            (UserVar "add"), TopLam (([(UserVar "x_boxed");(UserVar "y_boxed")],[],[(UserVar "x");(UserVar "y");(UserVar "r")],[]),            
+                Case(
+                    App((UserVar "x_boxed"), []),
+                    (UserVar "x_boxed"),
+                    AAlts (                       
+                        [  (UserVar "Int", [(UserVar "x")]),
+                            Case(
+                                App((UserVar "y_boxed"), []),
+                                (UserVar "x_boxed"),
+                                AAlts (
+                                    [(UserVar "Int",[(UserVar "y")]),
+                                        Case(
+                                            Prim [ALit Wasm.I32Add; AVar (UserVar "x"); AVar (UserVar "y")],
+                                            (UserVar "r"),
+                                            PAlts([], 
+                                                Constr (UserVar "Int", [AVar (UserVar "r")])))                                    
+                                    ],
+                                    Prim [ALit Wasm.Unreachable; ALit (I32Const 0)]
+                                )
+                            )
+                        ],
+                        Prim [ALit Wasm.Unreachable; ALit (I32Const 0)]
+                    )
+                ) 
+            )
+            (UserVar "subtract"), TopLam (([(UserVar "x_boxed");(UserVar "y_boxed")],[],[(UserVar "x");(UserVar "y");(UserVar "r")],[]),
+                Case(
+                    App((UserVar "x_boxed"), []),
+                    (UserVar "x_boxed"),
+                    AAlts (
+                        [(UserVar "Int",[(UserVar "x")]),
+                            Case(
+                                App((UserVar "y_boxed"), []),
+                                (UserVar "x_boxed"),
+                                AAlts (
+                                    [(UserVar "Int",[(UserVar "y")]),
+                                        Case(
+                                            Prim [ALit Wasm.I32Sub; AVar (UserVar "x"); AVar (UserVar "y"); ],
+                                            (UserVar "r"),
+                                            PAlts([], 
+                                                Constr (UserVar "Int", [AVar (UserVar "r")])))                                    
+                                    ],
+                                    Prim [ALit Wasm.Unreachable; ALit (I32Const 0)]
+                                )
+                            )
+                        ],
+                        Prim [ALit Wasm.Unreachable; ALit (I32Const 0)]
+                    )
+                )            
+            )
+            (UserVar "Fibonacci"),  TopLam (([(UserVar "x")],[],[(UserVar "x_boxed"); (UserVar "result"); (UserVar "return")],[]),
+                Case(
+                    Constr(UserVar "Int", [AVar (UserVar "x")]), 
+                    (UserVar "x_boxed"),
+                    PAlts ([],
+                        Case(
+                            Prim[AVar (UserVar "fibonacci_boxed");  AVar((UserVar "x_boxed"))],
+                            (UserVar "result"),
+                            AAlts(
+                                [(UserVar "Int", [(UserVar "return")]),
+                                    Prim [AVar (UserVar "return")]
+                                ],
+                                Prim [ALit Wasm.Unreachable; ALit (I32Const 0)]
+                            )
+                        )
+                    )
+                )               
+            )
+            (UserVar "fibonacci_boxed"), TopLam (([(UserVar "x_boxed")],[],[(UserVar "one"); (UserVar "two"); (UserVar "x_minus_one");(UserVar "x_minus_two");(UserVar "a");(UserVar "b"); (UserVar "x")],[]),
+                Case(
+                    App((UserVar "x_boxed"),[]),
+                    (UserVar "x_boxed"),
+                    AAlts(
+                        [(UserVar "Int", [(UserVar "x")]),
+                            Case(
+                                Prim[AVar (UserVar "x")], 
+                                (UserVar "x"),
+                                PAlts (
+                                    [
+                                        Wasm.I32Const 0, Constr (UserVar "Int", [ALit (Wasm.I32Const 1)])
+                                        Wasm.I32Const 1, Constr (UserVar "Int", [ALit (Wasm.I32Const 1)])
+                                    ],
+                                    Case(
+                                        Constr (UserVar "Int", [ALit (Wasm.I32Const 1)]),
+                                        (UserVar "one"),
+                                        PAlts(
+                                            [],
+                                            Case(
+                                                Constr (UserVar "Int", [ALit (Wasm.I32Const 2)]),
+                                                (UserVar "two"),
+                                                PAlts(
+                                                    [],
+                                                    Case(
+                                                        Prim[AVar(UserVar "subtract"); AVar (UserVar "x_boxed");  AVar (UserVar "one")],
+                                                        (UserVar "x_minus_one"),
+                                                        PAlts(
+                                                            [],
+                                                            Case(
+                                                                Prim[AVar(UserVar "subtract"); AVar (UserVar "x_boxed");  AVar (UserVar "two")],
+                                                                (UserVar "x_minus_two"),
+                                                                PAlts(
+                                                                    [],                                            
+                                                                    Case(
+                                                                        Prim[AVar (UserVar "fibonacci_boxed"); AVar (UserVar "x_minus_one")],
+                                                                        (UserVar "a"),
+                                                                        PAlts(
+                                                                            [],
+                                                                            Case(
+                                                                                Prim[AVar (UserVar "fibonacci_boxed");  AVar (UserVar "x_minus_two")],
+                                                                                (UserVar "b"),
+                                                                                PAlts([], 
+                                                                                    Prim[AVar(UserVar "add"); AVar (UserVar "a"); AVar (UserVar "b")])
+                                                                           )        
+                                                                        )     
+                                                                    )                                                      
+                                                                )
+                                                            )  
+                                                        )         
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        ],
+                        Prim [ALit Wasm.Unreachable; ALit (I32Const 0)]             
+                    )
+                )
+            )        
+        ]
     let fibonacciProgram =
         stgModule
         |> WasmGen.genProgram
